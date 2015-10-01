6ccd9c9ad9a7289e1a97bbb9921f160b337c6366
mkb0517 <brownmk@hawaii.edi>
2015-09-30 18:20:43 -1000

    issue 1 2 and 3 fixed

--- calculate_gc_content.py
+++ calculate_gc_content.py
@@ -8,24 +8,26 @@ a_count = 0
 c_count = 0
 g_count = 0
 t_count = 0
+total   = 0.0
 
 with open(sys.argv[1], 'r') as fasta:
     for line in fasta:
         if line.startswith(">"):
             # Header line, skip it
             continue
-        for base in line.strip():
-            if base == "A":
-                a_count += 1
-            elif base == "C":
-                c_count += 1
-            elif base == "G":
-                g_count += 1
-            else:
-                t_count += 1
+        total += len(line.strip())
+        a_count += line.count('A',0)
+        c_count += line.count('C',0)
+        g_count += line.count('G',0)
+        t_count += line.count('T',0)
+
+a_perc = (a_count/total)*100
+c_perc = (c_count/total)*100
+g_perc = (g_count/total)*100
+t_perc = (t_count/total)*100
 
 print("Base counts for file %s:" % sys.argv[1])
-print("A: %d" % a_count)
-print("C: %d" % c_count)
-print("G: %d" % g_count)
-print("T: %d" % t_count)
+print("A: %d (%d%s)" % (a_count, a_perc, '%'))
+print("C: %d (%d%s)" % (c_count, c_perc, '%'))
+print("G: %d (%d%s)" % (g_count, g_perc, '%'))
+print("T: %d (%d%s)" % (t_count, t_perc, '%'))
